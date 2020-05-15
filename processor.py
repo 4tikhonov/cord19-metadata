166018f1d6d1a35a9c311d096dbcccb0552b5f25
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2020-05-15 23:06:20 +0200

    V15 update

--- processor.py
+++ processor.py
@@ -9,6 +9,74 @@ class CORDProcessor():
         self.df = df
         self.directory = directory
         
+    def has_v15_metadata(self):
+        if self.directory:
+            jsfile = {}
+            self.papers = {}
+            self.rows = []
+            for i in self.df.index:
+                self.metadata = {}
+                doi = self.df.iloc[i].doi
+                relpath = self.df.iloc[i].pdf_json_files
+                section = (str(relpath) + "/") * 2
+                pmcid = self.df.iloc[i].pmcid
+                filename = self.directory + '/' + relpath
+                print("%s\n" % filename)
+                self.metadata['path'] = relpath
+                try:
+                    with open(filename) as paperjs:
+                        jsfile = json.load(paperjs)
+                except:
+                    print("Problem with", self.df.iloc[i].cord_uid)
+                    #continue
+
+                _id = self.df.iloc[i]["cord_uid"]
+                self.metadata['cord_uid'] = _id
+                #print(filename)
+                #print(jsfile)
+        
+                if "title" in jsfile.keys():
+                    self.rows.append(dict(cord_uid=_id, section="title", subsection=0, text=jsfile["title"]))
+                    self.metadata['title'] = jsfile["title"]
+                else:
+                    self.rows.append(dict(cord_uid=_id, section="title", subsection=0, text=self.df.iloc[i].title))
+                    self.metadata['title'] = self.df.iloc[i].title
+        
+                if "abstract" in jsfile.keys():
+                    if len(jsfile["abstract"]) > 1:
+                        for j in range(len(jsfile["abstract"])):
+                            self.metadata['abstract'] = jsfile["abstract"]
+                            self.rows.append(dict(cord_uid=_id, section="abstract", subsection=0, text=jsfile["abstract"][j]["text"]))
+                    else:
+                        self.metadata['abstract'] = jsfile["abstract"]
+                        self.rows.append(dict(cord_uid=_id, section="abstract", subsection=0, text=jsfile["abstract"]))
+                elif "abstract" in jsfile["metadata"].keys():
+                    self.rows.append(dict(cord_uid=_id, section="abstract", subsection=0, text=jsfile["metadata"]["abstract"]))
+                    self.metadata['abstract'] = jsfile["metadata"]["abstract"]
+                else:
+                    self.rows.append(dict(cord_uid=_id, section="abstract", subsection=0, text=self.df.iloc[i].abstract))
+                    self.metadata['abstract'] = self.df.iloc[i].abstract
+
+                sections = list(set([k["section"] for k in jsfile["body_text"]]))
+
+                for section in sections:
+                    self.metadata['body_text'] = jsfile["body_text"]
+                    for l in range(len(jsfile["body_text"])):
+                        if jsfile["body_text"][l]["section"] == section:
+                            if section == '':
+                                section = "body_text"
+                            self.rows.append(dict(cord_uid=_id, section=section,
+                                     subsection=l, text=jsfile["body_text"][l]["text"]))
+
+                c = CORDCommons(self.directory)
+                tables = c.extract_tables_from_json(jsfile)
+                self.metadata['tables'] = tables
+                for table in tables:
+                    self.rows.append(dict(cord_uid=_id, section=table[0], subsection=table[1], text=table[2]))
+                self.metadata['body_rows'] = self.rows
+                self.papers[doi] = self.metadata
+        return
+
     def has_pmc_xml_parse(self):
         if self.directory:
             jsfile = {}
