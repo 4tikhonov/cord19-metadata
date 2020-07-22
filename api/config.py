79932fe8c27b434d16a340a3161ef92f6103a02f
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2020-07-22 09:40:49 +0200

    Tested for v38 CORD-19

---
+++ api/config.py
@@ -0,0 +1,11 @@
+mongouser = 'coronawhyguest'
+mongopass = 'coro901na'
+mongohost = "localhost:27017"
+corddatabase = 'cord19'
+cordversion = 'v38'
+
+adminmongouser = 'admin'
+adminmongopassword = 'somemongoadminPASS'
+altcorddatabase = 'altmetrics'
+altmetricsID = '103'
+doindex = 'mongo'
